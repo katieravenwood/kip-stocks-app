56eb9873605c25c40a2ecdb38d8816a7bef93413
Katie Ravenwood <17629242+katieravenwood@users.noreply.github.com>
2022-04-17 01:21:46 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -32,9 +32,9 @@ df = yf.download(option,start= start_date,end= end_date, progress=False)
 # Bollinger Bands
 indicator_bb = BollingerBands(df['Close'])
 bb = df
-bb['bb_h'] = indicator_bb.bollinger_hband()
-bb['bb_l'] = indicator_bb.bollinger_lband()
-bb = bb[['Close','bb_h','bb_l']]
+bb['Upper (Overbought)'] = indicator_bb.bollinger_hband()
+bb['Lower (Oversold)'] = indicator_bb.bollinger_lband()
+bb = bb[['Close','Upper (Overbought)','Lower (Oversold)']]
 
 # Moving Average Convergence Divergence
 macd = MACD(df['Close']).macd()
