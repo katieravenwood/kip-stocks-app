0d85f17854824a388f9e77f58bf2809d574aff8a
Katie Ravenwood <17629242+katieravenwood@users.noreply.github.com>
2022-04-17 11:35:28 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -4,10 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-import datetime
-from datetime import date, timedelta
-today = date.today()
-
 import yfinance as yf # https://pypi.org/project/yfinance/
 from ta.volatility import BollingerBands
 from ta.trend import MACD
