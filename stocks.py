a07f2a53ecc1635a14d78e4ce2edbd0ea66e1595
Katie Ravenwood <17629242+katieravenwood@users.noreply.github.com>
2022-04-18 15:39:27 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -54,19 +54,19 @@ st.header(stock_name)
 st.subheader(stocklist)
 
 # Plot the prices and the bolinger bands
-st.write('Stock Bollinger Bands')
+st.subheader('Value with Bollinger Bands')
 st.line_chart(bb)
 
 progress_bar = st.progress(0)
 
 # Plot MACD
-st.write('Stock Moving Average Convergence Divergence (MACD)')
+st.subheader('Moving Average Convergence Divergence (MACD)')
 st.area_chart(macd)
 
 # Plot RSI
-st.write('Stock RSI ')
+st.subheader('Relative Strength Index (RSI)')
 st.line_chart(rsi)
 
 # Data of recent days
-st.write('Recent data ')
+st.subheader('Recent data ')
 st.dataframe(df.tail(10))
